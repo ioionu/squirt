8c31ffc7cb415612dde9a315bf040073a704f910
josh <io@ioio.nu>
2014-03-16 22:25:33 +1100

    set host to squirt.localhost

--- squirt.js
+++ squirt.js
@@ -1,8 +1,9 @@
 var sq = window.sq;
 sq.version = '0.0.1';
-sq.host =  window.location.search.match('sq-dev') ?
-  document.scripts[document.scripts.length - 1].src.match(/\/\/.*\//)[0]
-        : '//www.squirt.io/bm/';
+//sq.host =  window.location.search.match('sq-dev') ?
+//  document.scripts[document.scripts.length - 1].src.match(/\/\/.*\//)[0]
+//       : '//www.squirt.io/bm/';
+sq.host = '//squirt.localhost/';
 sq.analytics = false;
 
 (function(Keen){
